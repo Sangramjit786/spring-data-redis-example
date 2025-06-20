331447dd406138fa5246a4eaeb3bb4b943debdfe
Rohit <nathrohit48@gmail.com>
2025-06-20 11:46:41 +0530

    Spring Data Redis '@CacheEvict', '@Cacheable' and '@EnableCaching' annotation implementayion

--- src/main/java/com/javatechie/redis/controller/RadisController.java
+++ src/main/java/com/javatechie/redis/controller/RadisController.java
@@ -3,6 +3,8 @@ package com.javatechie.redis.controller;
 import com.javatechie.redis.entity.Product;
 import com.javatechie.redis.repository.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,10 +26,12 @@ public class RadisController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(key = "#id",value = "Product",unless = "#result.price > 1000")
     public Product findProduct(@PathVariable int id) {
         return dao.findProductById(id);
     }
     @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id",value = "Product")
     public String remove(@PathVariable int id)   {
         return dao.deleteProduct(id);
     }
